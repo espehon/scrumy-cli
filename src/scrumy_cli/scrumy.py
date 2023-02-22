84b95887654a27d3b8e44bd2b09afdf0e48f29e4
espehon <gregg.howlett@gmail.com>
2023-02-22 13:59:47 -0800

    more imports and proof of concept

--- src/scrumy_cli/scrumy.py
+++ src/scrumy_cli/scrumy.py
@@ -1,7 +1,10 @@
 # Copyright (c) 2023, espehon
 # License: https://www.gnu.org/licenses/gpl-3.0.html
 
+import sys
+import os
 import argparse
+import json
 import importlib.metadata
 
 
@@ -12,3 +15,22 @@ try:
     __version__ = f"scrumy {importlib.metadata.version('scrumy_cli')} from scrumy_cli"
 except importlib.metadata.PackageNotFoundError:
     __version__ = "Package not installed..."
+
+
+
+
+
+
+
+#DATA_PATH = os.path.expanduser('~/.local/share/scrumy/meetings.json')
+DATA_PATH = os.path.expanduser('~/github/scrumy/test/test.json')
+
+
+
+with open(DATA_PATH, 'r') as data:
+    meetings = json.load(data)
+
+
+
+for meeting in meetings:
+    print(meeting)
